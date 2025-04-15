9a572584c897fefce6aa3e353bc0b41252c44c31
IJAHARKHAN <khan7oct@gmail.com>
2025-04-15 22:25:39 +0530

    get latest

--- src/Components/TextForm.js
+++ src/Components/TextForm.js
@@ -54,7 +54,7 @@ function TextForm(props) {
         <div className="container">
         <div className="my-1">
         <h4 htmlFor="exampleFormControlTextarea1" className="form-label">{props.formText}</h4>
-        <textarea className="form-control" style={{ backgroundColor: props.mode === 'light' ? 'white' : 'black', color: props.mode === 'light' ? 'black' : 'white' }} id="exampleFormControlTextarea1" rows="5" value={text} onChange={(e) => {setText(e.target.value)}} placeholder='Enter Text..'></textarea>
+        <textarea className="form-control textarea" style={{ backgroundColor: props.mode === 'light' ? 'white' : 'black', color: props.mode === 'light' ? 'black' : 'white' }} id="exampleFormControlTextarea1" rows="5" value={text} onChange={(e) => {setText(e.target.value)}} placeholder='Enter Text..'></textarea>
         </div>
         <button className="btn btn-primary btn-sm my-1 me-1" onClick={handleUpperCase}>Convert To Upper Case</button>
         <button className="btn btn-primary btn-sm my-1 me-1" onClick={handleLowerCase}>Convert To Lower Case</button>
@@ -62,7 +62,8 @@ function TextForm(props) {
         <button className="btn btn-primary btn-sm my-1 me-1" onClick={handleRemoveExtraSpaces}>Remove Extra Spaces</button>
         <button className="btn btn-primary btn-sm my-1 me-1" onClick={handleClear}>Clear</button>
         <h5 className='my-2'>Your Text Summary</h5>
-        <p>{ text.length > 0 ? text.split(' ').length : '0'  } Words, {text.length} Characters</p>       
+        {/* <p>{ text.length > 0 ? text.split(' ').length : '0'  } Words, {text.length} Characters</p>   */}
+        <p>{ text.split(' ').filter((item)=>(item.length!==0)).length   } Words, {text.length} Characters</p>  
         <p>{0.008 * text.split(' ').length} Minutes Read</p>
         <h2>Preview</h2>
         <div>{text.length > 0 ? text : 'Enter text in text area.'}</div>
